90303c97bc209a38250c37d2d4b5ff59fa0d4dfe
WDushyantha <gitdushyantha@gmail.com>
2025-10-08 07:47:00 -0700

    Add Test case

--- todoapp/todotask/tests.py
+++ todoapp/todotask/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class TaskModelTest(TestCase):
+
+    def test_home_view_returns_200(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
